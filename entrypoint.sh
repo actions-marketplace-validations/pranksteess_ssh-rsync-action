ef10c3e3519f5868af9d7b3ff4c3f0ee63997537
specer <xhxpecer@gmail.com>
2020-10-30 12:24:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -43,12 +43,12 @@ executeSSH() {
     if [ -z "$COMMANDS" ]; then	
       COMMANDS="$COMMAND"	
     else	
-      COMMANDS="$COMMANDS&&$COMMAND"	
+      COMMANDS="$COMMANDS && $COMMAND"	
     fi	
   done <<< $LINES
   
-  echo "ssh -o StrictHostKeyChecking=no -p ${INPUT_PROXY_PORT:-22} $INPUT_PROXY_USER@$INPUT_PROXY_HOST ssh -o StrictHostKeyChecking=no -p ${INPUT_DST_PORT:-22} $INPUT_DST_USER@$INPUT_DST_HOST \"$COMMANDS\""
-  ssh -o StrictHostKeyChecking=no -p ${INPUT_PROXY_PORT:-22} $INPUT_PROXY_USER@$INPUT_PROXY_HOST ssh -o StrictHostKeyChecking=no -p ${INPUT_DST_PORT:-22} $INPUT_DST_USER@$INPUT_DST_HOST "$COMMANDS"
+  echo "ssh -o StrictHostKeyChecking=no -p ${INPUT_PROXY_PORT:-22} $INPUT_PROXY_USER@$INPUT_PROXY_HOST ssh -o StrictHostKeyChecking=no -p ${INPUT_DST_PORT:-22} $INPUT_DST_USER@$INPUT_DST_HOST \"${COMMANDS:0-2}\""
+  ssh -o StrictHostKeyChecking=no -p ${INPUT_PROXY_PORT:-22} $INPUT_PROXY_USER@$INPUT_PROXY_HOST ssh -o StrictHostKeyChecking=no -p ${INPUT_DST_PORT:-22} $INPUT_DST_USER@$INPUT_DST_HOST "${COMMANDS:0-2}"
  }
 
 executeRsync() {
